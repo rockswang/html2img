a6a003b609ea9b95747817fb6d5d4db0077d0be9
rockswang <rockswang@gmail.com>
2020-08-20 18:03:56 +0800

    首次提交

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# html2img
-Use puppeteer driven Headless Chrome to generate images for arbitary HTML
+# HTML2IMG
+Use puppeteer driven Headless Chrome to generate images for arbitary HTML<br/>
+
+使用puppeteer驱动Chrome无头浏览器将任意HTML内容渲染成图片
+
+### 调用方法:
+* 默认接口地址：`http://yourhost:50002`
+* 必须使用POST方法
+* 请求体中直接包含HTML文本
+* Content-Type可以是text/plain或者text/html
+* 接口URL后面可以添加下面的可选参数
+
+### 可选参数:
+|参数名|可选值|说明|
+----|----|----|
+|type|png/jpeg，默认png|图片格式:。<br/>* PNG支持透明半透明<br/>* JPEG体积更小
+|encoding|binary/base64，默认binary|编码格式。注意：<br/>* base64方式前面有base64类型头，形如`data:image/png;base64,...`
+
+### 构建说明:
+* 因为puppeteer中包含一个完整的Chromium浏览器（100多M），请务必使用cnpm或淘宝镜像安装模块
+* bunyan是本项目使用的日志模块，其构建需要node-gyp，如果构建不成功，可以去除依赖并自行替换log.xxx为console.log即可
+
+### 调用方法举例:
+```shell
+curl "http://localhost:50002/" -H "Content-Type: text/html" --data-binary "<html><style>#b{font:bold 60px 微软雅黑;line-height:60px;display:inline-block;background-color:black;padding:.2em .3em .2em .2em}#b div{display:inline-block;padding:.1em}#t1{color:white}#t2{margin-left:0.2em;color:black;background-color:orange;border-radius:.1em}</style><body><div id='b'><div id='t1'>搞点</div><div id='t2'>黄色</div></div></body></html>" --output aaa.png
+``` 
+
+### 其它说明
+* 安全考虑，HTML中不能包含任何script元素，style元素中不能包含任何外链！
+* 建议使用base64方式将图片编码在HTML正文中
+* 因为直接使用真实浏览器渲染，可以使用SVG等高级特性
+
+### 关于性能
+* 本项目预先启动一个无头浏览器在后台随时待命
+* 每次来新请求，使用池机制在现成页面上渲染HTML内容
+* 经测试，主要性能瓶颈在抓图步骤，这个跟运行平台有关，Windows尚可，Linux极佳；据说在MacOS上比较慢
+* 在Windows10（小米Pro笔记本） > 5FPS
+* Ubuntu18（腾讯云1核2G屌丝配置）> 15FPS
---
+++ index.js
@@ -0,0 +1,91 @@
+const bunyan = require('bunyan')
+const express = require('express')
+const bodyParser = require('body-parser')
+const puppeteer = require('puppeteer')
+const css = require('css')
+const cheerio = require('cheerio')
+
+const log = bunyan.createLogger({ name: 'html2img', streams: [{ level: 'debug', stream: process.stdout }] })
+
+const app = express()
+
+app.use(bodyParser.text({ type: 'text/*' }))
+app.post('/', async (req, res) => {
+  const { type = 'png', encoding = 'binary' } = req.query
+  const text = req.body
+  log.debug({ text }, '请求')
+  const err = checkHtml(text)
+  if (err) return res.status(400).send(err)
+  const img = await newHtml(text, type, encoding).catch(e => e)
+  if (img instanceof Error) {
+    return res.status(500).send(img.message + '\n' + img.stack)
+  }
+  if (img instanceof Buffer) {
+    return res.type(type).send(img)
+  }
+  res.type('text/plain').send(`data:image/${type};base64,` + img)
+})
+
+app.listen(50002, function () {
+  log.info('HTML2IMG服务开始监听50002端口')
+})
+
+function checkHtml (html) {
+  const $ = cheerio.load(html)
+  if ($('script').length) return '不能包含任何脚本元素'
+  let style = Array.from($('style')).map(s => $(s).html()).join('\n')
+  if (!style.trim()) return
+  try {
+    style = css.parse(style).stylesheet.rules
+    for (const r of style) {
+      if (!r.declarations) continue
+      for (const d of r.declarations) {
+        if (d.value && /http(s)?:\/\//.test(d.value)) return 'style元素中不能包含任何外部链接'
+      }
+    }
+  } catch (e) {
+    return e.message
+  }
+}
+
+let browser
+const pages = []
+
+async function newHtml (html, type, encoding) {
+  let id = pages.findIndex(p => !p.busy)
+  if (id < 0) {
+    id = pages.length
+    pages.push(await browser.newPage())
+  }
+  const page = pages[id]
+  page.busy = true
+  const tm = Date.now()
+  let error, res
+  try {
+    await page.setContent(`<html><body><div id="__html2img_container__" style="display:inline-block">${html}</div></body></html>`)
+    const clip = await page.evaluate(() => {
+      const element = document.querySelector('#__html2img_container__')
+      const { x, y, width, height } = element.getBoundingClientRect()
+      return { x, y, width, height }
+    })
+    res = await page.screenshot({ clip, type, encoding })
+  } catch (e) {
+    error = e
+  }
+  log.info({ html, type, encoding, error }, `在页面#${id}执行，耗时${Date.now() - tm}ms`)
+  page.busy = false
+  return res
+}
+
+;(async () => {
+  const tm = Date.now()
+  browser = await puppeteer.launch({
+    // headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+  log.info(`启动无头浏览器，耗时${Date.now() - tm}ms`)
+  process.on('exit', () => {
+    log.info('关闭浏览器')
+    browser.close()
+  })
+})()
