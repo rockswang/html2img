81817c19ed2b78dd1bd7a61faa447895d4f57dc8
rockswang <rockswang@gmail.com>
2020-09-04 17:22:50 +0800

    * 将一些写死的参数提取为常量，可自行修改 * 添加定时检查，自动关闭空闲页面的机制，以节省内存 * 可添加图片外链前缀白名单，允许添加一些安全且速度稳定的CDN

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const css = require('css')
 const cheerio = require('cheerio')
 
 const log = bunyan.createLogger({ name: 'html2img', streams: [{ level: 'debug', stream: process.stdout }] })
-
-const noCheck = process.argv.includes('--no-check')
+// 是否不检测HTML内容
+const NO_CHECK = process.argv.includes('--no-check')
+// 服务端口
+const PORT = 50002
+// 请求体大小上限
+const BODY_LIMIT = '4mb'
+// 图片src允许的地址前缀。图片总体是安全的，但是外链图片的下载速度将显著影响渲染速度
+const SAFE_PREFIX = [ // 允许base64及下列前缀的外部图片
+  'data:image/',
+  'https://cdn\\.jsdelivr\\.net/gh/' // jsdelivr有国内CDN，速度很快，因此可以把图片提交到github作为图床，通过jsdelivr获取
+]
+const safePrefix = new RegExp('^(' + SAFE_PREFIX.join('|') + ')')
 
 const app = express()
 
-app.use(bodyParser.text({ type: 'text/*', limit: '4mb' }))
+app.use(bodyParser.text({ type: 'text/*', limit: BODY_LIMIT }))
 app.post('/', async (req, res) => {
-  const { type = 'png', encoding = 'binary', __NO_CHECK__ = 0 } = req.query
+  const { type = 'png', encoding = 'binary' } = req.query
   const text = req.body
   log.debug({ text }, '请求')
-  const err = !noCheck && !__NO_CHECK__ && checkHtml(text)
+  const err = !NO_CHECK && checkHtml(text)
   if (err) return res.status(400).send(err)
   const img = await newHtml(text, type, encoding).catch(e => e)
   if (img instanceof Error) {
@@ -28,8 +38,8 @@ app.post('/', async (req, res) => {
   res.type('text/plain').send(`data:image/${type};base64,` + img)
 })
 
-app.listen(50002, function () {
-  log.info('HTML2IMG服务开始监听50002端口')
+app.listen(PORT, function () {
+  log.info(`HTML2IMG服务开始监听${PORT}端口`)
 })
 
 function checkHtml (html) {
@@ -38,14 +48,19 @@ function checkHtml (html) {
     if ($('script').length) throw new Error('不能包含任何脚本元素')
     if ($('link').length) throw new Error('不能包含任何外链资源')
     if ($('iframe').length || $('frame').length) throw new Error('不能包含frame/iframe')
-    if (Array.from($('img')).map(n => $(n).attr('src')).find(src => !/^data:image/.test(src))) throw new Error('不能包含任何外部图片')
+    for (const src of Array.from($('img')).map(n => $(n).attr('src'))) {
+      if (!safePrefix.test(src)) throw new Error('不允许的外部图片链接')
+    }
     let style = Array.from($('style')).map(s => $(s).html()).join('\n')
     if (!style.trim()) return
     style = css.parse(style).stylesheet.rules
     for (const r of style) {
       if (!r.declarations) continue
       for (const d of r.declarations) {
-        if (d.value && /http(s)?:\/\//.test(d.value)) throw new Error('style元素中不能包含任何外链资源')
+        if (!d.value || !/url\([^)]+\)/.test(d.value)) continue
+        const mch = /url\(([^)]+)\)/.exec(d.value)
+        const url = ['"', "'"].includes(mch[1].charAt(0)) ? mch[1].slice(1, -1) : mch[1]
+        if (!safePrefix.test(url)) throw new Error('style元素中包含不允许的外部资源')
       }
     }
   } catch (e) {
@@ -57,13 +72,15 @@ let browser
 const pages = []
 
 async function newHtml (html, type, encoding) {
-  let id = pages.findIndex(p => !p.busy)
-  if (id < 0) {
-    id = pages.length
-    pages.push(await browser.newPage())
+  let idx = pages.findIndex(p => p.idle)
+  if (idx < 0) {
+    idx = pages.length
+    const pg = await browser.newPage()
+    pg.__id__ = '' + Date.now() + (100 + ~~(Math.random() * 900))
+    pages.push(pg)
   }
-  const page = pages[id]
-  page.busy = true
+  const page = pages[idx]
+  page.idle = false
   const tm = Date.now()
   let error, res
   try {
@@ -80,8 +97,8 @@ async function newHtml (html, type, encoding) {
   } catch (e) {
     error = e
   }
-  log.info({ html, type, encoding, error }, `在页面#${id}执行，耗时${Date.now() - tm}ms`)
-  page.busy = false
+  log.info({ html, type, encoding, error }, `在页面#${page.__id__}执行，耗时${Date.now() - tm}ms`)
+  page.idle = Date.now()
   return res
 }
 
@@ -92,6 +109,18 @@ async function newHtml (html, type, encoding) {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     defaultViewport: { width: 4000, height: 6000 }
   })
+  setInterval(() => { // 检查空闲过久的页面并关闭，以节省内存
+    for (var i = pages.length; --i >= 0;) {
+      const p = pages[i]
+      if (!p.idle) return
+      const life = Date.now() - p.idle
+      if (life > 300000) {
+        log.info(`页面#${p.__id__}已经超过${life}ms没有任务了，关闭该页面`)
+        p.close()
+        pages.splice(i, 1)
+      }
+    }
+  }, 60000)
   log.info(`启动无头浏览器，耗时${Date.now() - tm}ms`)
   process.on('exit', () => {
     log.info('关闭浏览器')
